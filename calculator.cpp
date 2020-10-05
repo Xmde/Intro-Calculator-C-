3b6c5139da89bd3b8f628b23bae5d514989002a3
Eric <erve2005@gmail.com>
2020-10-05 13:29:57 -0700

    Did the recurstion black magic. Most of it works and I think that most of it is done

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
+double calculator(string equation){
+    int operationIndexFirst = -1;
+    int operationIndexSecond = -1;
+
+    
+    operationIndexFirst = equation.find_last_of("+");
+    operationIndexSecond = equation.find_last_of("-");
+
+    if((operationIndexFirst != operationIndexSecond) && (operationIndexFirst > operationIndexSecond)){
+        //cout << equation.substr(0,operationIndexFirst) << "  +  " << equation.substr(operationIndexFirst+1) << "\n";
+        return calculator(equation.substr(0,operationIndexFirst)) + calculator(equation.substr(operationIndexFirst+1));
+    }
+
+    if((operationIndexFirst != operationIndexSecond) && (operationIndexFirst < operationIndexSecond)){
+       // cout << equation.substr(0,operationIndexSecond) << "  -  " << equation.substr(operationIndexSecond+1) << "\n";
+        return calculator(equation.substr(0,operationIndexSecond)) - calculator(equation.substr(operationIndexSecond+1));
+    }
+
+    operationIndexFirst = equation.find_last_of("*");
+    operationIndexSecond = equation.find_last_of("/");
+
+    if((operationIndexFirst != operationIndexSecond) && (operationIndexFirst > operationIndexSecond)){
+        //cout << equation.substr(0,operationIndexFirst) << "  *  " << equation.substr(operationIndexFirst+1) << "\n";
+        return calculator(equation.substr(0,operationIndexFirst)) * calculator(equation.substr(operationIndexFirst+1));
+    }
+
+    if((operationIndexFirst != operationIndexSecond) && (operationIndexFirst < operationIndexSecond)){
+        //cout << equation.substr(0,operationIndexSecond) << "  /  " << equation.substr(operationIndexSecond+1) << "\n";
+        return calculator(equation.substr(0,operationIndexSecond)) / calculator(equation.substr(operationIndexSecond+1));
+    }
+
+    return stod(equation);
+}
+
+
 int main(){
-string in;
-cout << "enter equation ";
-cin >> in;
-cout << in << " " << "done";
+string input;
+cout << "Enter Equation: ";
+cin >> input;
+cout << calculator(input);
 }
